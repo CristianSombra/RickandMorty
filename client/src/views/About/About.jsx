447ef6ceae928b66707a3bee5c4e6f830d6baff1
CristianSombra <cristiansombra87@gmail.com>
2023-10-30 06:30:14 -0300

    Actualización estilos y responsividad: se realiza deploy

--- client/src/views/About/About.jsx
+++ client/src/views/About/About.jsx
@@ -3,10 +3,10 @@ import "./About.css";
 
 function About () {
     return (
-        <div class="card-container">
+        <div class="about-container">
            <div class="row"> 
-            <div class="card">
-                <div class="card-body">
+            <div>
+                <div class="card-body card-about">
                     <h1 class="card-title">Proyecto Integrador Rick and Morty</h1>
                     <h2 class="card-subtitle mb-2">Bootcamp Henry</h2>
                     <p>Éste proyecto se fue armando al realizar todas las homeworks de cada módulo cursado. Desde el Frontend hasta el Backend, se trabajó sobre los estilos, servidor y creación de base de datos. Sin embargo, éste pequeño proyecto cuenta con actualizaciones como código mas estructurado y aplicación de estilo mejorado y mediante Bootstrap se le pudo dar responsividad. Orgulloso de mi primer proyecto.</p>
